69b4d299aeb910f551fdb05c714c31e8abdd393c
rheemansum <rheewalt@gmail.com>
2016-11-04 18:14:24 -0500

    design changes

--- seed.js
+++
@@ -1,5 +0,0 @@
-var xlsx = require('xlsx');
-var workbook = xlsx.readFile('pipe.xlsx');
-var sheetNames = workbook.SheetNames;
-var pipeNPSData = xlsx.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
-console.log(test);
